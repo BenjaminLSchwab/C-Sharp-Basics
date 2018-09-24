2fb187cc49e8eb9a8aab2ce1e226fce028b706b8
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 15:34:55 -0700

    shipping drill

---
+++ Shipping/Shipping/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Shipping
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            float weight = Convert.ToSingle(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            float width = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            float height = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            float length = Convert.ToSingle(Console.ReadLine());
+
+            if (width + length + height > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+
+            float quote = ((width + length + height) * weight) / 100;
+
+            Console.WriteLine("Your estimated total for shipping this package is: $" + Math.Round(quote,2));
+            Console.ReadLine();
+        }
+    }
+}
