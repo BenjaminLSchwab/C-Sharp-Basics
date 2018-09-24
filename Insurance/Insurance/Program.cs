5866ec7133b169cb3f8f8f45e9ac5b8ed87dc6c4
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 14:51:53 -0700

    insurance drill

---
+++ Insurance/Insurance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            byte tickets = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && !hasDUI && tickets < 4);
+
+            Console.ReadLine();
+        }
+    }
+}
