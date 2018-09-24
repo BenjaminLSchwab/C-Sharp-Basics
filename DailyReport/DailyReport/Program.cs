16acea29fd5fd64035842f85609a0ecd4c8cbc80
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 14:03:21 -0700

    daily report drill

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            byte courseNum = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string feedbackPositive = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedbackOther = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            byte hoursStudied = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
