8d5ea4730b2452b72cbf6073a32a80d1c9d2ef5a
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 15:47:24 -0700

    loops drill

---
+++ Loops1/Loops1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What color is a duck?");
+            bool correctAnswer = (Console.ReadLine() == "yellow");
+            while (!correctAnswer)
+            {
+                Console.WriteLine("Thats not quite right, guess again.");
+                correctAnswer = (Console.ReadLine() == "yellow");
+            }
+
+            do
+            {
+                Console.WriteLine("What if the duck was green? What color would he be then?");
+                correctAnswer = (Console.ReadLine() == "green");
+            } while (!correctAnswer);
+
+            Console.WriteLine("That makes sense to me.");
+            Console.ReadLine();
+        }
+    }
+}
