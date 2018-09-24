41ff82582da92af293d865689e0370ae18401e89
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 13:38:54 -0700

    Sample app for testing compiler, ConsoleApp1 for printing text to screen

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
