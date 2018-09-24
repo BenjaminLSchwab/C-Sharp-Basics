d5a7b00c7aad6630990b743215d23ddcafbf6828
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-24 14:27:45 -0700

    math exercise

---
+++ MathExercise/MathExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("1. Input a number...");
+            float input = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(input * 50);
+
+            Console.WriteLine("2. Input a number...");
+            input = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(input + 25);
+
+            Console.WriteLine("3. Input a number...");
+            input = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(input/12.5f);
+
+            Console.WriteLine("4. Input a number...");
+            input = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(input > 50);
+
+            Console.WriteLine("5. Input a number...");
+            input = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine(input % 7);
+
+            Console.ReadLine();
+        }
+    }
+}
