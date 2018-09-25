fe8b13fbe7bee3a4aede443bf0c46f7cfd54aff4
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-25 14:51:11 -0700

    began trycatch drill

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numerators = {5, 10, 15, 16, 27, 36, 54, 128, 293, 40112};
+
+            Console.WriteLine("Input a number to be used in several divisions...");
+
+            try
+            {
+                float input = Convert.ToSingle(Console.ReadLine());
+            }
+            catch(FormatException ex)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
