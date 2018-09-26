b54cbda8f95410d3d32ab57c1b20b62bdc479e35
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-26 15:47:47 -0700

    methods drill

---
+++ Methods1/Methods1/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1
+{
+    static public class Operations
+    {
+        static public int AddThree(int input)
+        {
+            return input + 3;
+        }
+
+        static public int OneMoreThanHalf(int input)
+        {
+            return (input / 2) + 1;
+        }
+
+        static public int PrettyClose(int input)
+        {
+            Random random = new Random();
+            return input + random.Next(-5, 5);
+        }
+
+    }
+}
---
+++ Methods1/Methods1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace Methods1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("No time to explain, give me a number.");
+            bool validInput = false;
+            int input = 0;
+            while (!validInput)
+            {
+                try
+                {
+                input = Convert.ToInt32(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Thats not a whole number, man. Try again.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("Number after 1st operation:");
+            input = Operations.AddThree(input);
+            Console.WriteLine(Convert.ToString(input));
+
+            Thread.Sleep(200);
+
+            Console.WriteLine("Number after 2nd operation:");
+            input = Operations.OneMoreThanHalf(input);
+            Console.WriteLine(Convert.ToString(input));
+
+            Thread.Sleep(200);
+
+            Console.WriteLine("Number after 3rd operation:");
+            input = Operations.PrettyClose(input);
+            Console.WriteLine(Convert.ToString(input));
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
