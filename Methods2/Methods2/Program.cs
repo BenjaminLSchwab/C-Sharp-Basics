fcbf850cd587f4673226f63c9e31915ea649804f
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-27 10:45:45 -0700

    methods2 drill

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main()
+        {
+            TwoInt twoInt = new TwoInt();
+
+            twoInt.DoMath(15, 20);
+
+            twoInt.DoMath(first: 60, second: 15);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Methods2/Methods2/TwoInt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    class TwoInt
+    {
+        public void DoMath(int first, int second)
+        {
+            first += 10;
+
+            Console.WriteLine(second);
+        }
+    }
+}
