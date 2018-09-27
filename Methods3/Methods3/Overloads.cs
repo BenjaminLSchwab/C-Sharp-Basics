d83c4c150ccc16ecb058540451a2764750d76898
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-27 11:09:05 -0700

    methods3 drill

---
+++ Methods3/Methods3/Overloads.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods3
+{
+    class Overloads
+    {
+        public int DoMath(int input)
+        {
+            return input * 2;
+        }
+
+        public int DoMath(decimal input)
+        {
+            return (int)(input + 5);
+        }
+
+        public int DoMath(string input)
+        {
+            int ans = 0;
+
+            try
+            {
+                ans = Convert.ToInt32(input);
+            }
+            catch
+            {
+                Console.WriteLine("Can't do math on words");
+                return 0;
+            }
+
+            ans = ans % 2;
+            return ans;
+        }
+
+
+    }
+}
---
+++ Methods3/Methods3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Methods3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Overloads overloads1 = new Overloads();
+            int firstInput = 2;
+            Console.WriteLine(overloads1.DoMath(firstInput));
+
+            Overloads overloads2 = new Overloads();
+            decimal secondInput = 4.32M;
+            Console.WriteLine(overloads2.DoMath(secondInput));
+
+            Overloads overloads3 = new Overloads();
+            string thirdInput = "5";
+            Console.WriteLine(overloads3.DoMath(thirdInput));
+
+
+            Console.ReadLine();
+        }
+    }
+}
