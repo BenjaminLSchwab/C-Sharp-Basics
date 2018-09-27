23f49476e174d4be1687a7d709001c46f01c44f8
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-09-27 11:37:21 -0700

    methods 4 drill

---
+++ Methods4/Methods4/AverageTwo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods4
+{
+    class AverageTwo
+    {
+        public int Average(int first, int second = 1)
+        {
+            return (first + second) / 2;
+        }
+    }
+}
---
+++ Methods4/Methods4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main()
+        {
+            AverageTwo averageTwo = new AverageTwo();
+
+            int firstInput = 0;
+            int secondInput = 0;
+            string tempInput = "";
+
+            bool validInput = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Input a whole number please.");
+                    firstInput = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Second input is optional, type a whole number if you feel like it, then press enter.");
+                    tempInput = Console.ReadLine();
+                    if (tempInput != "") Convert.ToInt32(tempInput); //this will throw an error and re-loop if user inputs a non whole number
+                    
+                    validInput = true;
+                }
+                catch
+                {
+                    validInput = false;
+                }
+
+
+            } while (!validInput);
+
+
+
+            if (tempInput == "")
+            {
+                Console.WriteLine("Averaged with 1: {0}", averageTwo.Average(firstInput));
+            }
+            else
+            {
+                secondInput = Convert.ToInt32(tempInput);
+                Console.WriteLine("Average: {0}", averageTwo.Average(firstInput, secondInput));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
