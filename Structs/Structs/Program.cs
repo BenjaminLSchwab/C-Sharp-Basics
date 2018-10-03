ca863fd0ad25c3a52d9d31565c40e32176fbbd60
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-02 17:05:38 -0700

    structs drill

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 2;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
