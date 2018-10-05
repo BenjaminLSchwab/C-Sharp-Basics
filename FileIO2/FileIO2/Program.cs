0e70becfe659112426cdb1b306ce931184a435dc
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-05 12:18:54 -0700

    datetime drill

---
+++ FileIO2/FileIO2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Input a number or face the consequences.");
+            bool validInput = false;
+            string input = "";
+            int number = 0;
+            while (!validInput)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    number = Convert.ToInt32(input);
+                    validInput = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Whole numbers only!");
+                    validInput = false;
+                }
+            }
+
+            TimeSpan hours = TimeSpan.FromHours(number);
+
+            Console.WriteLine("In {0} hours, it will be {1}", number, DateTime.Now + hours);
+            Console.ReadLine();
+        }
+    }
+}
