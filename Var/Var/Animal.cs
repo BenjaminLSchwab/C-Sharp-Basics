27694e03ef4e929b42a8e17e8f72790b0f5f78f2
BenjaminLSchwab <benjamin.schwab.nonspam@gmail.com>
2018-10-08 11:00:47 -0700

    Var Drill

---
+++ Var/Var/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    class Animal
+    {
+        public Animal(string name) : this(name, 4) { }
+
+        public Animal(string name, int numLegs)
+        {
+            Name = name;
+            NumLegs = numLegs;
+        }
+
+        public string Name { get; set; }
+        public int NumLegs { get; set; }
+    }
+}
---
+++ Var/Var/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string zooName = "The Best Zoo";
+            Animal bird = new Animal("Flaps the bird", 2);
+            Animal squirrel = new Animal("Nutty the squirrel");
+            var animals = new List<Animal>() {bird, squirrel };
+
+
+            Console.WriteLine("Welcome to {0}", zooName);
+            Console.WriteLine("Animals:");
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine("Name: {0}, Number of legs: {1}", animal.Name, animal.NumLegs);
+            }
+
+            Console.WriteLine("Wow, we really need to get some more animals...");
+            Console.ReadLine();
+        }
+    }
+}
